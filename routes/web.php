8bb2f7e2cf94543857b6114db485ebcfa80e4fe9
2460245313@qq.com <vagrant@homestead>
2020-05-22 17:43:16 +0000

    用户注册页面

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,13 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+//主页
 Route::get('/', 'StaticPagesController@home')->name('home');
+//帮助页
 Route::get('/help', 'StaticPagesController@help')->name('help');
+//关于页面
 Route::get('/about', 'StaticPagesController@about')->name('about');
+
+//注册
+Route::get('sigup','UserController@create')->name('sigup');
+//登录
