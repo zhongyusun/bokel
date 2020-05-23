6ed270aa44b43ec9ec8c34505faf64829c680f1b
2460245313@qq.com <vagrant@homestead>
2020-05-23 13:46:13 +0000

    用户退出登录

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -26,6 +26,12 @@ class SessionsController extends Controller
     			return redirect()->back()->withInput();
     	}
     	return;
+    }
 
+    public function destroy(){
+
+    	Auth::logout();
+    	session()->flash('success','您已成功退出');
+    	return redirect('login');
     }
 }
