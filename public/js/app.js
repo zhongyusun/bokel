0be9cff98e2c42fa404b1eec81dbd22d1374a422
2460245313@qq.com <vagrant@homestead>
2020-05-23 13:41:11 +0000

    登录成功后的导航逻辑

--- public/js/app.js
+++ public/js/app.js
@@ -37227,6 +37227,8 @@ module.exports = function(module) {
 
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
+__webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -3,6 +3,7 @@
   <head>
     <title>@yield('title', 'Weibo App') - Laravel 入门教程</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
   </head>
   <body>
 
@@ -15,5 +16,6 @@
         @include('layouts._foot')
       </div>
     </div>
+    <script type="text/javascript" src="{{mix('js/app.js')}}"></script>
   </body>
 </html>
