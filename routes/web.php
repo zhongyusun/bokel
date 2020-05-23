da7504a90d6384f0b13713f34762627878829d42
2460245313@qq.com <vagrant@homestead>
2020-05-23 07:16:50 +0000

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,16 @@ Route::get('/about', 'StaticPagesController@about')->name('about');
 
 //注册
 Route::get('sigup','UserController@create')->name('sigup');
+
+
+Route::resource('users','UserController');
+//等于
+//Route::get('/users','UsersController@index')->name('users.index');
+// Route::get('/users/create', 'UsersController@create')->name('users.create');
+// Route::get('/users/{user}', 'UsersController@show')->name('users.show');
+// Route::post('/users', 'UsersController@store')->name('users.store');
+// Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
+// Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+// Route::delete('/users/{user}', 'UsersController@destroy')->name('users.destroy');
+
 //登录
