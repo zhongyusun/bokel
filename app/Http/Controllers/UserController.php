ef2fe41e362354ed03b16a9b2955dea155846a22
2460245313@qq.com <vagrant@homestead>
2020-05-24 07:02:03 +0000

    更改用户资料

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -33,4 +33,28 @@ class UserController extends Controller
          session()->flash('success','欢迎，您将在这里开启一段新的旅程~');
          return redirect()->route('users.show',[$user]);
     }
+
+    public function edit(User $user){
+    	return view('users.edit',compact('user'));
+    }
+
+
+    public function update(User $user,Request $request){
+    	$this->validate($request,[
+    		'name'=>'required|max:50',
+    		'password'=>'required|confirmed|min:6'
+
+    	]);
+    	$date=[];
+    	$date['name']=$request->name;
+    	if ($request->password) {
+    		$date['password']=bcrypt($request->password);
+    	}
+    	$user->update($date);
+    	
+    	session()->flash('success','个人资料更新成功');
+
+    	return redirect()->route('users.show',$user->id);
+
+    }
 }
