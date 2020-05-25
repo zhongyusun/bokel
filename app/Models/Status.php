0c98c8b59f569e37ec9122f7c02bfe6844dbf3af
2460245313@qq.com <vagrant@homestead>
2020-05-25 13:28:29 +0000

    发布微博

--- app/Models/Status.php
+++ app/Models/Status.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
+
+	protected $fillable = ['content'];
     //定义文章和用户的关联
     public function user(){
     	return $this->belongsTo(User::class);
